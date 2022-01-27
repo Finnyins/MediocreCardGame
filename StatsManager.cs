3893292ca64d40e5a279d7f1a40249c858aa7383
Finnyins <mataoasis@gmail.com>
2022-01-27 08:46:45 -0700

    * Added new Card textures. * Changed the "Holographic" card's name to "Irridescent" and added a replacement "Holographic" card. * Version indicator on the main menu now shows revision number * Updated the "Secondary" card's placeholder name to "Contrast". The database manager now checks for the oudated "Secondary" card data in the database, and corrects it if it exists.

--- StatsManager.cs
+++ StatsManager.cs
@@ -26,7 +26,8 @@ namespace Project24
                 conn.Execute("insert or ignore into Stats (GameMode) values ('Custom')");
                 conn.Execute("create table if not exists Foils (ID INTEGER NOT NULL UNIQUE, Name TEXT NOT NULL UNIQUE, File TEXT, PRIMARY KEY(ID AUTOINCREMENT))");
                 conn.Execute("insert or ignore into Foils (Name, File) values ('Default', '/Resources/FL_Default.png')");
-                conn.Execute("insert or ignore into Foils (Name, File) values ('Secondary', '/Resources/FL_Secondary.png')");
+                conn.Execute("update or ignore Foils set Name = 'Contrast', File = '/Resources/FL_Contrast.png' where Name = 'Secondary'");
+                conn.Execute("insert or ignore into Foils (Name, File) values ('Contrast', '/Resources/FL_Contrast.png')");
             }
         }
 
